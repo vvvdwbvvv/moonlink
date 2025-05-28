4a154d6fdf5215b828c45f8c4c073a92d905d394
dentiny <dentinyhao@gmail.com>
2025-05-27 23:53:45 -0700

    Update devcontainer to use dockerfile (#269)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,11 +5,7 @@ git config devcontainers-theme.show-dirty 1
 echo alias psql=\'psql -h localhost -U postgres\' >> ~/.bashrc
 
 # rust precommit hook requirements.
-sudo apt update -y
-cargo install cargo-sort
 rustup component add clippy
 
 # precommit hook requirement.
-sudo apt install -y python3-pip
-pip3 install pre-commit --break-system-packages
 pre-commit install -t pre-push
