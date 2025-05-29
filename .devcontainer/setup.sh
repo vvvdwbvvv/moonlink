2bf8c62d3e4035f727125925f96f2e90ede0f531
dentiny <dentinyhao@gmail.com>
2025-05-28 21:55:14 -0700

    Improve devcontainer setup (#278)
    * Improve devcontainer condif
    
    * fix shell

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,5 @@ git config devcontainers-theme.show-dirty 1
 # pg access.
 echo alias psql=\'psql -h localhost -U postgres\' >> ~/.bashrc
 
-# rust precommit hook requirements.
-rustup component add clippy
-
 # precommit hook requirement.
 pre-commit install -t pre-push
