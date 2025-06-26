bccc1a6be4672daf0e8e5afd3390d54dde354e7d
dentiny <dentinyhao@gmail.com>
2025-06-26 11:53:28 -0700

    Fix psql command at devcontainer startup (#618)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 git config devcontainers-theme.show-dirty 1
 
 # pg access.
-echo alias psql=\'psql -h localhost -U postgres\' >> ~/.bashrc
+echo "alias psql='PGPASSWORD=postgres psql -h postgres -p 5432 -U postgres -d postgres'" >> ~/.bashrc
 
 # precommit hook requirement.
 pre-commit install -t pre-push
