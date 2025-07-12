6351fdf6f8015c0c00a3c6f5225e7c75ffe26ef4
dentiny <dentinyhao@gmail.com>
2025-07-12 13:28:14 -0700

    [minor] Add error logging for iceberg error (#841)

--- src/moonlink/src/storage/iceberg/tokio_retry_utils.rs
+++ src/moonlink/src/storage/iceberg/tokio_retry_utils.rs
@@ -1,11 +1,16 @@
 /// This module contains tokio retry related util functions.
+///
+/// TODO(hjiang): Switch to [`backon`](https://github.com/Xuanwo/backon).
 use iceberg::Error as IcebergError;
 use tokio_retry2::RetryError as TokioRetryError;
+use tracing::error;
 
 /// Convert iceberg error to tokio retry error. Only `Unexpected` iceberg error translates to transient error.
 pub(crate) fn iceberg_to_tokio_retry_error(err: IcebergError) -> TokioRetryError<IcebergError> {
     match err.kind() {
         iceberg::ErrorKind::Unexpected | iceberg::ErrorKind::CatalogCommitConflicts => {
+            // Only logging on retriable error, non-retriable ones will be handled by error propagation, should avoid double handling.
+            error!("Encounter retriable iceberg error: {err}");
             TokioRetryError::Transient {
                 err,
                 retry_after: None,
