0b232c068d7e3c7a7c2cf96347ccb3e061ca8b56
dentiny <dentinyhao@gmail.com>
2025-07-30 21:11:24 -0700

    [storage] Implement default deserialization value (#1154)

--- src/moonlink/src/storage/filesystem/accessor_config.rs
+++ src/moonlink/src/storage/filesystem/accessor_config.rs
@@ -11,9 +11,16 @@ use serde::{Deserialize, Serialize};
 ///
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct RetryConfig {
+    #[serde(default = "RetryConfig::default_max_count")]
     pub max_count: usize,
+
+    #[serde(default = "RetryConfig::default_min_delay")]
     pub min_delay: std::time::Duration,
+
+    #[serde(default = "RetryConfig::default_max_delay")]
     pub max_delay: std::time::Duration,
+
+    #[serde(default = "RetryConfig::default_delay_factor")]
     pub delay_factor: f32,
 }
 
@@ -22,6 +29,20 @@ impl RetryConfig {
     const DEFAULT_MAX_DELAY: std::time::Duration = std::time::Duration::from_secs(5);
     const DEFAULT_DELAY_FACTOR: f32 = 1.5;
     const DEFAULT_MAX_COUNT: usize = 5;
+
+    // Util functions for serde defaults.
+    fn default_max_count() -> usize {
+        Self::DEFAULT_MAX_COUNT
+    }
+    fn default_min_delay() -> std::time::Duration {
+        Self::DEFAULT_MIN_DELAY
+    }
+    fn default_max_delay() -> std::time::Duration {
+        Self::DEFAULT_MAX_DELAY
+    }
+    fn default_delay_factor() -> f32 {
+        Self::DEFAULT_DELAY_FACTOR
+    }
 }
 
 impl Default for RetryConfig {
@@ -65,12 +86,17 @@ impl ChaosConfig {
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct TimeoutConfig {
     /// Timeout for all attempts for an IO operations, including retry.
+    #[serde(default = "TimeoutConfig::default_timeout")]
     pub timeout: std::time::Duration,
 }
 
 impl TimeoutConfig {
     /// Default timeout for all IO operations.
     const DEFAULT_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(60);
+
+    fn default_timeout() -> std::time::Duration {
+        Self::DEFAULT_TIMEOUT
+    }
 }
 
 impl Default for TimeoutConfig {
@@ -123,6 +149,7 @@ impl AccessorConfig {
 mod tests {
     use super::*;
     use crate::StorageConfig;
+
     use serde_json::json;
 
     /// Testing scenario: deserialize accessor config with only storage config populated.
@@ -144,6 +171,39 @@ mod tests {
                     root_directory: "/tmp".to_string()
                 },
                 retry_config: RetryConfig::default(),
+                timeout_config: TimeoutConfig::default(),
+                chaos_config: None,
+            }
+        );
+    }
+
+    /// Testing scenario: only one field is specified for retry config.
+    #[test]
+    fn test_deserialize_retry_config() {
+        let input = json!({
+            "storage_config": {
+                "FileSystem": {
+                    "root_directory": "/tmp"
+                },
+            },
+            "retry_config": {
+                "delay_factor": 2
+            }
+        });
+
+        let config: AccessorConfig = serde_json::from_value(input).unwrap();
+        assert_eq!(
+            config,
+            AccessorConfig {
+                storage_config: StorageConfig::FileSystem {
+                    root_directory: "/tmp".to_string()
+                },
+                retry_config: RetryConfig {
+                    delay_factor: 2.0,
+                    max_count: RetryConfig::default_max_count(),
+                    min_delay: RetryConfig::default_min_delay(),
+                    max_delay: RetryConfig::default_max_delay(),
+                },
                 timeout_config: TimeoutConfig::default(),
                 chaos_config: None,
             }
