8f14241dfb31c1b64da8d64330f4c816af0a8f08
fweilun <william20020503@gmail.com>
2025-08-09 20:20:29 +0800

    Configure default Parquet writer settings (#1316)

--- src/moonlink/src/storage/parquet_utils.rs
+++ src/moonlink/src/storage/parquet_utils.rs
@@ -4,9 +4,14 @@ use parquet::{basic::Compression, file::properties::WriterProperties};
 /// Default compression.
 const DEFAULT_COMPRESSION: Compression = parquet::basic::Compression::SNAPPY;
 
+// Default row group size from duckdb.
+const DEFAULT_ROW_GROUP_SIZE: usize = 122880;
+
 pub(crate) fn get_default_parquet_properties() -> WriterProperties {
     WriterProperties::builder()
         .set_compression(DEFAULT_COMPRESSION)
         .set_dictionary_enabled(true)
+        .set_dictionary_page_size_limit(DEFAULT_ROW_GROUP_SIZE / 100)
+        .set_writer_version(parquet::file::properties::WriterVersion::PARQUET_1_0)
         .build()
 }
