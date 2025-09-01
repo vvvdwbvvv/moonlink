4be359526847b4238a5595466be0f99ed5e9116f
dentiny <dentinyhao@gmail.com>
2025-09-01 09:31:48 -0700

    Add timestamp to logging (#1848)

--- src/moonlink_backend/src/logging.rs
+++ src/moonlink_backend/src/logging.rs
@@ -2,14 +2,15 @@ use tracing_subscriber::Layer;
 
 pub fn init_logging() {
     use tracing_subscriber::{
-        fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry,
+        fmt, fmt::time, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry,
     };
 
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
     let fmt_layer = fmt::layer()
+        .with_timer(time::ChronoLocal::new("%Y-%m-%d %H:%M:%S%:z".to_string()))
         .with_test_writer()
-        .with_ansi(false)
+        .with_ansi(true)
         .with_filter(env_filter);
 
     // Compose the subscriber.
