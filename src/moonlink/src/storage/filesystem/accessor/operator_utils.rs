b16c9cee22bf06a29705c1657a9be56b915d4079
dentiny <dentinyhao@gmail.com>
2025-09-10 00:41:50 -0700

    [storage] Disable config load for S3 access (#1995)

--- src/moonlink/src/storage/filesystem/accessor/operator_utils.rs
+++ src/moonlink/src/storage/filesystem/accessor/operator_utils.rs
@@ -51,7 +51,8 @@ fn create_opendal_operator_impl(storage_config: &StorageConfig) -> Result<Operat
                 .bucket(bucket)
                 .endpoint("https://storage.googleapis.com")
                 .access_key_id(access_key_id)
-                .secret_access_key(secret_access_key);
+                .secret_access_key(secret_access_key)
+                .disable_config_load();
             Ok(Operator::new(builder)?.finish())
         }
         #[cfg(feature = "storage-s3")]
@@ -66,7 +67,8 @@ fn create_opendal_operator_impl(storage_config: &StorageConfig) -> Result<Operat
                 .bucket(bucket)
                 .region(region)
                 .access_key_id(access_key_id)
-                .secret_access_key(secret_access_key);
+                .secret_access_key(secret_access_key)
+                .disable_config_load();
             if let Some(endpoint) = endpoint {
                 builder = builder.endpoint(endpoint);
             }
